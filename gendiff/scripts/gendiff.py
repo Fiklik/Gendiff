eaa0f959e0da5b25354d098dd66babed3f92c4f5
Aleksey Yablunev <yablunev.av@gmail.com>
2023-08-17 05:02:36 +0300

    completed third step on the project

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,7 @@
+#!usr/bin/env python3
 import argparse
+import json
+
 
 
 parser = argparse.ArgumentParser(
@@ -9,6 +12,32 @@ parser.add_argument('second_file', type=str)
 parser.add_argument('-f', '--format', help='set format of output')
 args = parser.parse_args()
 
+def generate_diff(file_path1, file_path2):
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+
+    keys = list(set(list(file1) + list(file2)))
+    keys.sort()
+    result = '{\n'
+    for key in keys:
+        if key in file1 and key in file2:
+            if file1[key] != file2[key]:
+                result += f'  - {key}: {str(file1[key])}\n'
+                result += f'  + {key}: {str(file2[key])}\n'
+            else:
+                result += f'    {key}: {str(file1[key])}\n'
+        elif key in file1 and key not in file2:
+            result += f'  - {key}: {str(file1[key])}\n'
+        elif key in file2 and key not in file1:
+            result += f'  + {key}: {str(file2[key])}\n'
+    result += '}'
+    return result
+
+
+def main():
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
 
 if __name__ == '__main__':
-    print('Hello')
+    main()
---
+++ main.py
@@ -0,0 +1,5 @@
+from gendiff.scripts.gendiff import generate_diff
+
+
+if __name__ == '__main__':
+    generate_diff()
