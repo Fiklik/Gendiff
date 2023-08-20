f82eb133ecd6c0990d0e04692c6de13d0f167f29
Aleksey Yablunev <yablunev.av@gmail.com>
2023-08-20 11:08:47 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,10 +3,14 @@ import argparse
 import json
 
 
-def generate_diff(file_path1, file_path2):
+def get_files_from_paths(file_path1, file_path2):
     with open(file_path1) as file1, open(file_path2) as file2:
         file1 = json.load(file1)
         file2 = json.load(file2)
+        return file1, file2
+
+
+def generate_diff(file1, file2):
     file1_keys = list(file1)
     file2_keys = list(file2)
     keys = list(set(file1_keys + file2_keys))
@@ -35,7 +39,9 @@ def main():
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file)
+    file1, file2 = get_files_from_paths(args.first_file, args.second_file)
+
+    diff = generate_diff(file1, file2)
     print(diff)
 
 
