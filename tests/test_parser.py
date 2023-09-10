f3c3ad67ceebc7ac00bcdb9faec6ae57ef44d57b
Aleksey Yablunev <yablunev.av@gmail.com>
2023-09-10 21:03:08 +0300

    completed step 6. added recursive comparison

--- tests/test_parser.py
+++
@@ -1,9 +0,0 @@
-# import pytest
-# import itertools
-# from gendiff.scripts.parser import parse
-# from gendiff.scripts.diff import get_difference_between_files
-# from gendiff.formatters.format import format_diff
-
-
-# @pytest.fixture
-# def parse_yml():
